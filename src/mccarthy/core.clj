f639953b343a2f135153a90ed35f3ec81730a847
kordano <konrad.kuehne@rocketmail.com>
2015-05-13 18:40:30 +0200

    rename and add keywords n stuff

--- src/mccarthy/core.clj
+++ src/mccarthy/core.clj
@@ -1,7 +1,7 @@
 (ns mccarthy.core
   (:require [instaparse.core :refer [parser]]))
 
-(def scheme-parser
-  (parser (slurp "resources/scheme-grammar.bnf")))
+(def scheme-parser (parser (slurp "resources/scheme-grammar.bnf")))
 
 (scheme-parser "(+ (+ 1 2) 1123 2123)")
+(scheme-parser "(def sum (fn (x y) (+ x y)))")
