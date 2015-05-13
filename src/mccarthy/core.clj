948562399e067b241949a20bf15453d3c6ce7130
kordano <konrad.kuehne@rocketmail.com>
2015-05-13 19:43:13 +0200

    add visualization and more keywords

--- src/mccarthy/core.clj
+++ src/mccarthy/core.clj
@@ -1,7 +1,19 @@
 (ns mccarthy.core
-  (:require [instaparse.core :refer [parser]]))
+  (:require [instaparse.core :refer [parser visualize]]))
 
-(def scheme-parser (parser (slurp "resources/scheme-grammar.bnf")))
+(def parse (parser (slurp "resources/scheme-grammar.bnf")))
 
-(scheme-parser "(+ (+ 1 2) 1123 2123)")
-(scheme-parser "(def sum (fn (x y) (+ x y)))")
+(parse "1")
+(parse "\"asdasd    asd  asd as d\"")
+(parse "fn")
+(parse "def")
+(parse "list")
+
+(parse "(inc 1)")
+(parse "(+ (+ 1 2) 1123 2123)")
+(parse "(/ 2 1)")
+(parse "(def sum (fn (x y) (+ x y)))")
+
+(parse "(list 12 23 123 )")
+
+(parse "")
